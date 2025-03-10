00a056d6831fadeea557cc389b46434a056a5549
Alexis Lasselin <alexis.lasselin@algosup.com>
2025-03-10 18:04:43 +0100

    transform videos by frame

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import cv2
+import os
+
+def video_to_frames(video, path_output_dir):
+    # extract frames from a video and save to directory as 'x.png' where 
+    # x is the frame index
+    vidcap = cv2.VideoCapture(video)
+    count = 0
+    while vidcap.isOpened():
+        success, image = vidcap.read()
+        if success:
+            cv2.imwrite(os.path.join(path_output_dir, '%d.png') % count, image)
+            count += 1
+        else:
+            break
+    cv2.destroyAllWindows()
+    vidcap.release()
+
+video_to_frames('videos/test.mp4', 'frames/')
